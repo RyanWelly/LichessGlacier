01a11228e4370384efad4d0f0415feca458492ac
UncountableHollows <uncountablehollows@gmail.com>
2022-08-24 03:06:30 +1200

    intial commit

---
+++ glacier.js
@@ -0,0 +1,16 @@
+//TODOs: Remove ultra/hyper/bullet tournaments from tournament list
+// Add ability to set cooldowns for how long you want to be blocked from these time controls.
+// Maybe add ability to set "reward periods" where these time controls are unblocked for 
+
+
+let OnePlusZeroButton = document.querySelector('div[data-id^="1+0"]')
+OnePlusZeroButton.remove()
+
+let TwoPlusOneButton = document.querySelector('div[data-id^="2+1"]')
+TwoPlusOneButton.remove()
+
+let ThreePlusZero = document.querySelector('div[data-id^="3+0"]')
+ThreePlusZero.remove()
+
+//remove lobby tab and custom game button.
+//let LobbyTab = document.querySelector
