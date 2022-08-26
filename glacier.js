be1dc6122eef1cbf206af8b1e8661661601cc7e1
UncountableHollows <uncountablehollows@gmail.com>
2022-08-26 21:40:11 +1200

    Removed create challenge button, removed corr tab (since clicking corr tab reloads the bullet button back).

--- glacier.js
+++ glacier.js
@@ -1,7 +1,7 @@
 //TODOs: Remove ultra/hyper/bullet tournaments from tournament list
 // Add ability to set cooldowns for how long you want to be blocked from these time controls.
 // Maybe add ability to set "reward periods" where these time controls are unblocked for 
-
+// Make the changes persist.
 
 let OnePlusZeroButton = document.querySelector('div[data-id^="1+0"]')
 OnePlusZeroButton.remove()
@@ -12,5 +12,19 @@ TwoPlusOneButton.remove()
 let ThreePlusZero = document.querySelector('div[data-id^="3+0"]')
 ThreePlusZero.remove()
 
-//remove lobby tab and custom game button.
-//let LobbyTab = document.querySelector
+let LobbyTab = document.querySelectorAll('span[role^="tab"]')[1]
+LobbyTab.remove()
+
+//add event listener on the other two lobby tabs to automatically call all these removals again.
+
+
+let CorrTab = document.querySelectorAll('span[role^="tab"]')[1]
+CorrTab.remove() //temp fix; get changes to persist even when user can click on corr tab.
+ 
+
+
+
+
+let CustomGameButton = document.querySelector('a[class^="button button-metal config_hook"]')
+CustomGameButton.remove() //This persists, the other removals don't.
+
