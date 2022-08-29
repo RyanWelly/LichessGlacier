83f026af85bc6865e99e6f2e8e55ea8e443e069a
UncountableHollows <uncountablehollows@gmail.com>
2022-08-29 17:02:20 +1200

    Pushing create new custom game button no longer causes bullet buttons to reappear

--- glacier.js
+++ glacier.js
@@ -3,28 +3,33 @@
 // Maybe add ability to set "reward periods" where these time controls are unblocked for 
 // Make the changes persist.
 
-let OnePlusZeroButton = document.querySelector('div[data-id^="1+0"]')
-OnePlusZeroButton.remove()
-
-let TwoPlusOneButton = document.querySelector('div[data-id^="2+1"]')
-TwoPlusOneButton.remove()
-
-let ThreePlusZero = document.querySelector('div[data-id^="3+0"]')
-ThreePlusZero.remove()
-
-let LobbyTab = document.querySelectorAll('span[role^="tab"]')[1]
-LobbyTab.remove()
-
-//add event listener on the other two lobby tabs to automatically call all these removals again.
 
 
-let CorrTab = document.querySelectorAll('span[role^="tab"]')[1]
-CorrTab.remove() //temp fix; get changes to persist even when user can click on corr tab.
- 
+function removeButtons() {
+    let OnePlusZeroButton = document.querySelector('div[data-id^="1+0"]')
+    OnePlusZeroButton.remove()
+
+    let TwoPlusOneButton = document.querySelector('div[data-id^="2+1"]')
+    TwoPlusOneButton.remove()
+
+    let ThreePlusZero = document.querySelector('div[data-id^="3+0"]')
+    ThreePlusZero.remove()
+
+    let LobbyTab = document.querySelectorAll('span[role^="tab"]')[1]
+    LobbyTab.remove()
+
+    //add event listener on the other two lobby tabs to automatically call all these removals again.
+
+
+    let CorrTab = document.querySelectorAll('span[role^="tab"]')[1]
+    CorrTab.remove()
+    console.log("test")
+}
+
+
+removeButtons()
 
 
 
 
-let CustomGameButton = document.querySelector('a[class^="button button-metal config_hook"]')
-CustomGameButton.remove() //This persists, the other removals don't.
 
